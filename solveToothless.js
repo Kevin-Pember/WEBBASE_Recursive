60d8a68d7dbd855080cd41036b91813a83adab16
Kevin-Pember <Kevinkzp@gmail.com>
2022-01-14 09:24:30 -0800

    Larger changes

--- solveToothless.js
+++ solveToothless.js
@@ -1,220 +1,252 @@
 solveInpr("1+sin(2)+9");
 
-//create an array full of object that contain name, equation, parse the equation, needs rad of deg, etc.
-function solveInpr(equation, returnTarget){
-    var foundTrig = [];
-    let funcList = [
-        "acsc",
-        "asec",
-        "acot",
-        "asin",
-        "acos",
-        "atan",
-        "sin",
-        "cos",
-        "tan",
-        "csc",
-        "sec",
-        "cot",
-        "ln",
-        "log",
-        "mod",
-        "d→f"
-      ];
-      let subEquation = equation;
-      for (let i = 0; i < equation.length; i++) {
-        if (funcMatch(subEquation)) {
-          if (subEquation.indexOf(func) > -1) {
-            let innerRAW = parEncap(subEquation.substring(subEquation.indexOf(func) + func.length));
-            let inner = recrSolve(innerRAW.substring(1, innerRAW.length - 1));
-            let funcTemp = findMethod(func);
-            subEquation = subEquation.substring(0, subEquation.indexOf(func))+subEquation.substring(subEquation.indexOf(func) + func.length+innerRAW.length);
-            let parsedFunc = assembly(funcTemp, inner);
-            equation = equation.replace(func + innerRAW, parsedFunc);
-            console.log("%c"+equation, "color: green");
-          }
-        }
-      }
-      foundTrig.sort(function (a, b) {
-        let returnVal = -1;
-        if (a.index > b.index) {
-          returnVal = 1;
-        }
-        return returnVal;
-      });
+//create an array full of object that contain name, equation, parse the equation, needs rad of deg, length, or  etc.
+function solveInpr(equation, returnTarget) {
+  var foundTrig = [];
+  let funcList = [
+    {
+      func: "acsc",
+      funcParse: ["Math.asin(1/", "v1", ")","toRad"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 4,
+    },
+    {
+      func: "asec",
+      funcParse: ["Math.acos(1/", "v1", ")", "toRad"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 4,
+    },
+    {
+      func: "acot",
+      funcParse: ["Math.atan(1/", "v1", ")", "toRad"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 4,
+    },
+    {
+      func: "asin",
+      funcParse: ["Math.asin(", "v1", ")", "toRad"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 4,
+    },
+    {
+      func: "acos",
+      funcParse: ["Math.acos(", "v1", ")", "toRad"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 4,
+    },
+    {
+      func: "atan",
+      funcParse: ["Math.atan(", "v1", ")", "toRad"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 4,
+    },
+    {
+      func: "sin",
+      funcParse: ["Math.sin(", "v1", ")", "toDeg"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 3,
+    },
+    {
+      func: "cos",
+      funcParse: ["Math.cos(", "v1", ")", "toDeg"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 3,
+    },
+    {
+      func: "tan",
+      funcParse: ["Math.tan(", "v1", ")", "toDeg"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 3,
+    },
+    {
+      func: "csc",
+      funcParse: ["Math.asin(1/", "v1", ")", "toDeg"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 3,
+    },
+    {
+      func: "sec",
+      funcParse: ["Math.acos(1/", "v1", ")", "toDeg"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 3,
+    },
+    {
+      func: "cot",
+      funcParse: ["Math.atan(1/", "v1", ")", "toDeg"],
+      inputs: 1,
+      funcRadDeg: true,
+      funcLength: 3,
+    },
+    {
+      func: "ln",
+      funcParse: ["Math.log(", "v1", ")"],
+      inputs: 1,
+      funcRadDeg: false,
+      funcLength: 2,
+    },
+    {
+      func: "log",
+      funcParse: ["Math.log10(", "v1", ")"],
+      inputs: 1,
+      funcRadDeg: false,
+      funcLength: 3,
+    },
+    {
+      func: "mod",
+      funcParse: ["v1 % v2"],
+      inputs: 2,
+      funcRadDeg: false,
+      funcLength: 3,
+    },
+  ];
+  let subEquation = equation;
+  for (let i = 0; i < equation.length; i++) {
+    if (funcMatch(subEquation) != "") {
+      let func = getByName(funcMatch(subEquation));
+      let innerRAW = parEncap(
+        subEquation.substring(func.funcLength)
+      );
+      let values = recrSolve(innerRAW.substring(1, innerRAW.length - 1),func);
+      let funcTemp = findMethod(func);
+      /*subEquation =
+        subEquation.substring(0, subEquation.indexOf(func)) +
+        subEquation.substring(
+          subEquation.indexOf(func) + func.length + innerRAW.length
+        );*/
+      let parsedFunc = assembly(func, funcTemp, values);
+      equation = equation.substring(0, i)+parsedFunc+equation.substring(i+func.funcLength+innerRAW.length);
+      subEquation = equation.substring(i+func.funcLength+innerRAW.length);
+      console.log("%c" + equation, "color: green");
+    }
+  }
+  foundTrig.sort(function (a, b) {
+    let returnVal = -1;
+    if (a.index > b.index) {
+      returnVal = 1;
+    }
+    return returnVal;
+  });
 }
-function funcMatch(equation){
-  let func4 = ["acsc","asec","acot","asin","acos","atan"];
-  if(func4.includes(equation.substring(0,5))){
-    return true;
-  }
-  let func3 = ["sin","cos","tan","csc","sec","cot","log","mod","d→f"];
-  if(func3.includes(equation.substring(0,4))){
-    return true;
-  }
-  let func2 = ["ln"];
-  if(func2.includes(equation.substring(0,3))){
-    return true;
+function getByName(name) {
+  for (let func in funcList) {
+    if (func.name == name) {
+      return func;
+    }
   }
 }
-function findMethod(func){
-  let funcTemp = [];
+function funcMatch(equation) {
+  for (let func in funcList) {
+    let check = equation.substring(0, func.funcLength + 1);
+    if (check == func.name) {
+      return func.name;
+    }
+  }
+  return "";
+}
+function findMethod(func) {
   let degRad = true;
-  switch (func) {
-    case "sin":
-      if (degRad) {
-        funcTemp = ["Math.sin(", "v", "*(Math.PI/180))"];
-      } else {
-        funcTemp = ["Math.sin(", "v", ")"];
+  if (func.funcRadDeg) {
+    if(degRad){
+      if(func.funcParse.includes("toDeg")){
+        return func.funcParse.splice(func.funcParse.indexOf("toDeg"), 1, "*(Math.PI/180)");
+      }else{
+        return func.funcParse.splice(func.funcParse.indexOf("toRad"), 1, "*(180/Math.PI)");
       }
-      break;
-    case "cos":
-      if (degRad) {
-        funcTemp = ["Math.cos(", "v", "*(Math.PI/180))"];
-      } else {
-        funcTemp = ["Math.cos(", "v", ")"];
-      }
-      break;
-    case "tan":
-      if (degRad) {
-        funcTemp = ["Math.tan(", "v", "*(Math.PI/180))"];
-      } else {
-        funcTemp = ["Math.tan(", "v", ")"];
-      }
-      break;
-    case "asin":
-      if (degRad) {
-        funcTemp = ["Math.asin(", "v", ")*(180/Math.PI)"];
-      } else {
-        funcTemp = ["Math.asin(", "v", ")"];
-      }
-      break;
-    case "acos":
-      if (degRad) {
-        funcTemp = ["Math.acos(", "v", ")*(180/Math.PI)"];
-      } else {
-        funcTemp = ["Math.acos(", "v", ")"];
-      }
-      break;
-    case "atan":
-      if (degRad) {
-        funcTemp = ["Math.atan(", "v", ")*(180/Math.PI)"];
-      } else {
-        funcTemp = ["Math.atan(", "v", ")"];
-      }
-      break;
-    case "csc":
-      if (degRad) {
-        funcTemp = ["1/Math.sin(", "v", "*(Math.PI/180))"];
-      } else {
-        funcTemp = ["1/Math.sin(", "v", ")"];
-      }
-      break;
-    case "sec":
-      if (degRad) {
-        funcTemp = ["1/Math.cos(", "v", "*(Math.PI/180))"];
-      } else {
-        funcTemp = ["1/Math.cos(", "v", ")"];
-      }
-      break;
-    case "cot":
-      if (degRad) {
-        funcTemp = ["1/Math.tan(", "v", "*(Math.PI/180))"];
-      } else {
-        funcTemp = ["1/Math.tan(", "v", ")"];
-      }
-      break;
-    case "acsc":
-      if (degRad) {
-        funcTemp = ["Math.asin(1/", "v", ")*(180/Math.PI)"];
-      } else {
-        funcTemp = ["Math.asin(1/", "v", ")"];
-      }
-      break;
-    case "asec":
-      if (degRad) {
-        funcTemp = ["Math.acos(1/", "v", ")*(180/Math.PI)"];
-      } else {
-        funcTemp = ["Math.acos(1/", "v", ")"];
-      }
-      break;
-    case "acot":
-      if (degRad) {
-        funcTemp = ["Math.atan(1/", "v", ")*(180/Math.PI)"];
-      } else {
-        funcTemp = ["Math.atan(1/", "v", ")"];
-      }
-      break;
-    case "ln":
-      funcTemp = ["Math.log(", "v", ")"];
-      break;
-    case "log":
-      funcTemp = ["Math.log10(", "v", ")"];
-      break;
-    case "mod":
-      funcTemp = ["v", "%", "v"];
-    break;
+    }else {
+      return func.funcParse;
+    }
+  }else{
+    return func.funcParse;
   }
-  return funcTemp;
 }
-function assembly(array, inner){
-  let returnString = "";
-  for( let x of array){
-    if(x == "v"){
+function assembly(func, parsedFunc, values) {
+  inputs = func.inputs;
+  for (let i = 1; i <= inputs; i++) {
+    parsedFunc = parsedFunc.replace("v" + i, values[i - 1]);
+  }
+  return parsedFunc;
+  /*let returnString = "";
+  for (let x of array) {
+    if (x == "v") {
       returnString += inner;
-    }else{
+    } else {
       returnString += x;
     }
   }
-  return returnString;
+  return returnString;*/
 }
-function recrSolve(equation){
-  return equation;
+function recrSolve(equation,func) {
+  let inputs = func.inputs;
+  if(inputs == 1){
+    return [equation];
+  }else {
+    let values = [];
+    for(let i = 1; i <= inputs; i++){
+      if(i != inputs){
+        values.push(equation.substring(0, equation.indexOf(",")));
+        equation = equation.substring(equation.indexOf(",") + 1);
+      }else {
+        values.push(equation.substring(0, equation.indexOf(")")));
+      }
+    }
+      return values
+  }
 }
 function parComplete(input) {
-    for (let i = 0; i < input.length; i++) {
-      if (input.charAt(i) == "(") {
-        if (
-          i + parEncap(input.substring(i)).length >= input.length &&
-          input.charAt(input.length - 1) != ")"
-        ) {
-          console.log("par coimplete complete par");
-          input += ")";
-        }
+  for (let i = 0; i < input.length; i++) {
+    if (input.charAt(i) == "(") {
+      if (
+        i + parEncap(input.substring(i)).length >= input.length &&
+        input.charAt(input.length - 1) != ")"
+      ) {
+        console.log("par coimplete complete par");
+        input += ")";
       }
     }
-    return input;
   }
-  function parEncap(sub) {
-    for (let i = 1; i < sub.length; i++) {
-      if (sub.charAt(i) == "(") {
-        i = i + parEncap(sub.substring(i)).length;
-      } else if (sub.charAt(i) == ")") {
-        sub = sub.substring(0, i + 1);
-        break;
-      } else if (i == sub.length - 1) {
-        sub = sub + ")";
-        break;
+  return input;
+}
+function parEncap(sub) {
+  for (let i = 1; i < sub.length; i++) {
+    if (sub.charAt(i) == "(") {
+      i = i + parEncap(sub.substring(i)).length;
+    } else if (sub.charAt(i) == ")") {
+      sub = sub.substring(0, i + 1);
+      break;
+    } else if (i == sub.length - 1) {
+      sub = sub + ")";
+      break;
+    }
+  }
+  return sub;
+}
+function funcIndex(func, equation, funcList) {
+  let has = false;
+  var hasVal = -1;
+  for (let i = funcList.length - 1; i >= 0; i--) {
+    if (funcList[i].func == func) {
+      has = true;
+      console.log(funcList[i].index);
+      if (hasVal < funcList[i].index) {
+        hasVal = funcList[i].index + funcList[i].func.length;
       }
     }
-    return sub;
   }
-  function funcIndex(func, equation, funcList) {
-    let has = false;
-    var hasVal = -1;
-    for (let i = funcList.length - 1; i >= 0; i--) {
-      if (funcList[i].func == func) {
-        has = true;
-        console.log(funcList[i].index);
-        if (hasVal < funcList[i].index) {
-          hasVal = funcList[i].index + funcList[i].func.length;
-        }
-      }
-    }
-    console.log("Has is " + has + " hasVal index is " + hasVal);
-    if (has) {
-      return hasVal + equation.substring(hasVal).indexOf(func);
-    } else {
-      return equation.indexOf(func);
-    }
+  console.log("Has is " + has + " hasVal index is " + hasVal);
+  if (has) {
+    return hasVal + equation.substring(hasVal).indexOf(func);
+  } else {
+    return equation.indexOf(func);
   }
+}
